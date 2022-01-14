56159a78aa43c23b1beff4348398dbf647e43d9a
cktews <cartertews25@gmail.com>
2022-01-14 10:20:44 -0600

    Workflow outline, Dialog box framework, gs conversion

--- perfusion.py
+++ perfusion.py
@@ -47,3 +47,15 @@ gsImg = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)   # Convert to grayscale
 # I don't believe contrast adjustment is needed since we are only
 # creating a perfusion index and there is some abiguity in that function,
 # but we can always add it.
+
+""" 
+Workflow:
+    1) Adjust contrast
+    2) Average filtering
+    3) Binarize
+    4) Remove Background:
+        a) Create Structuring Element
+        b) Close Image
+        c) Subtract background from image
+        d) Invert if needed
+"""
