1502b860cf9f4b52587b81a64c12958495b0913a
John Moses <mosesjohn2810@gmail.com>
2022-01-22 14:33:33 -0600

    added some comments to previous code

--- gui.py
+++ gui.py
@@ -3,13 +3,15 @@ from PIL import ImageTk, Image
 
 root = Tk()
 root.title('Testing GUI')
+#filepath on my computer, a filepath is only needed if the photo is not running in the same location as the code
 # root.iconbitmap('C:\Users\Johns\Desktop\School\BME 590\SeniorDesign')
 
+#tells software what photo to open, would like to learn how to make this so the user doesn't have to name their photo's 'test.jpg'
 myImage = ImageTk.PhotoImage(Image.open("test.jpg"))
 myLabel = Label(image = myImage)
 myLabel.pack()
 
-
+#button that allows the user to exit the program
 buttonExit = Button(root, text = "Exit Program", command = root.quit)
 buttonExit.pack()
 
