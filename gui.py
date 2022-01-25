b428eeaba6339384cd38525b4387c6dc6c4877e9
grboehringer <g2boehringer@gmail.com>
2022-01-25 09:31:08 -0600

    import image

--- gui.py
+++ gui.py
@@ -1,20 +1,44 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import filedialog as fd
 
 root = Tk()
 root.title('Testing GUI')
 #filepath on my computer, a filepath is only needed if the photo is not running in the same location as the code
 # root.iconbitmap('C:\Users\Johns\Desktop\School\BME 590\SeniorDesign')
 
-#tells software what photo to open, would like to learn how to make this so the user doesn't have to name their photo's 'test.jpg'
-myImage = ImageTk.PhotoImage(Image.open("test.jpg"))
-myLabel = Label(image = myImage)
-myLabel.pack()
+#finding the filename
+def select_file():
+    
+    # define allowed file types
+    filetypes = (
+        ('Image files', '*.jpg'),
+        ('All files', '*.*')
+    )
+
+        # prompt dialog box
+    filename = fd.askopenfilename(
+        title='Open an image',
+        initialdir='/',
+        filetypes=filetypes)
+        
+    return filename
+
+def importImg(filename):
+    #tells software what photo to open
+    #myImage = ImageTk.PhotoImage(Image.open(filename))
+    #myLabel = Label(image = myImage)
+    #myLabel.pack()
+
+    image = Image.open(filename)
+    image.show()
 
 #button that allows the user to exit the program
 buttonExit = Button(root, text = "Exit Program", command = root.quit)
 buttonExit.pack()
 
+filename = select_file()
+importImg(filename)
 
 
 
--- perfusion.py
+++ perfusion.py
@@ -1,25 +1,3 @@
 import cv2
 import numpy as np
 
-# import image
-I = select_file()
-Img = cv2.imread(I)                         # import image
-
-# image conversion and adjustments
-gsImg = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)   # Convert to grayscale
-
-# I don't believe contrast adjustment is needed since we are only
-# creating a perfusion index and there is some abiguity in that function,
-# but we can always add it.
-
-""" 
-Workflow:
-    1) Adjust contrast
-    2) Average filtering
-    3) Binarize
-    4) Remove Background:
-        a) Create Structuring Element
-        b) Close Image
-        c) Subtract background from image
-        d) Invert if needed
-"""
