2a455e1377881f1ec7ad16eb49d18d4859bfda4b
John Moses <mosesjohn2810@gmail.com>
2022-01-27 09:21:20 -0600

    I updated theSelect_file function to add the image to the GUI

--- gui.py
+++ gui.py
@@ -1,45 +1,40 @@
+import tkinter as tk
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
-from tkinter import filedialog as fd
 
-root = Tk()
+
+perIndex = 55.5
+
+root = tk.Tk()
+root.geometry("410x300")
 root.title('Testing GUI')
-#filepath on my computer, a filepath is only needed if the photo is not running in the same location as the code
-# root.iconbitmap('C:\Users\Johns\Desktop\School\BME 590\SeniorDesign')
+
+intro = tk.Label(root, text = 'Upload Files and Display')
+upload = tk.Button(root, text = 'Upload Files', width = 20, command =lambda:select_file())
 
 #finding the filename
 def select_file():
-    
+
     # define allowed file types
-    filetypes = (
-        ('Image files', '*.jpg'),
-        ('All files', '*.*')
-    )
+    f_types = [('Jpg files', '*.jpg'), ('PNG files','*.png')]
 
-        # prompt dialog box
-    filename = fd.askopenfilename(
-        title='Open an image',
-        initialdir='/',
-        filetypes=filetypes)
-        
-    return filename
-
-def importImg(filename):
-    #tells software what photo to open
-    #myImage = ImageTk.PhotoImage(Image.open(filename))
-    #myLabel = Label(image = myImage)
-    #myLabel.pack()
-
-    image = Image.open(filename)
-    image.show()
-
-#button that allows the user to exit the program
-buttonExit = Button(root, text = "Exit Program", command = root.quit)
-buttonExit.pack()
-
-filename = select_file()
-importImg(filename)
+    # prompt dialog box
+    filename = tk.filedialog.askopenfilename( filetypes = f_types)
+    #dynamic column variable
+    #image resizing before displaying
+    img = Image.open(filename)
+    img = img.resize((300,300))
+    #image display code
+    # Use this code if no image resizing is wanted/needed: img = ImageTk.PhotoImage(file = filename)
+    img = ImageTk.PhotoImage(img)
+    size = tk.Label(root)
+    size.grid(row = 3, column = 1)
+    size.image = img
+    size['image'] = img
+# Grid layout for GUI
+intro.grid(row = 1, column = 1, columnspan = 4)
+upload.grid(row = 2, column = 1, columnspan = 4)
 
 
-
-root.mainloop()
+root.mainloop() #allows the window to stay open
