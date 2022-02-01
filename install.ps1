8ab4e4982808b4e92700b10a66a764fe495b8895
grboehringer <g2boehringer@gmail.com>
2022-02-01 08:23:05 -0600

    install files

---
+++ install.ps1
@@ -0,0 +1,4 @@
+python -m pip install -U pip
+pip install pillow
+pip install numpy
+pip install opencv-python
---
+++ install.sh
@@ -0,0 +1,4 @@
+python -m pip install -U pip
+pip3 install pillow
+pip3 install numpy
+pip3 install opencv-python
