e6e2f550f68d6210de30f87a645a42b3c515f4cf
bjfriz <bjfriz@att.net>
2022-02-05 12:20:14 -0600

    fixed GUI layout and added a button for save (doesn't do anything at the moment)

--- gui.py
+++ gui.py
@@ -3,6 +3,32 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
 
+def start_window():
+    intial_img = Image.open("test.jpg")
+    intial_img = intial_img.resize((512,512))
+    intial_img = ImageTk.PhotoImage(intial_img)
+    size = tk.Label(root)
+    size.grid(row = 0, column = 0, columnspan=2, rowspan=40,padx=10,pady=10)
+    size.image = intial_img
+    size['image'] = intial_img
+
+    intro = tk.Label(root, text = 'Upload Files and Display', bg ='#3A3B3C', fg = 'white')
+    intro.grid(row = 0, column = 2, padx=5, sticky = "s")
+
+    upload = tk.Button(root, text = 'Upload Files', width = 20, bg ='#3A3B3C', fg = 'white', command =lambda:open_image())
+    upload.grid(row = 1, column = 2, padx=5, sticky = "n")
+
+    threshold_intensity.grid(row=5, column=2, padx=5, sticky = "s")
+    intensity_thresh_entry.grid(row=6, column=2, padx=5, sticky = "n")
+
+    diff_threshold.grid(row=8, column=2, padx=5, sticky = "s")
+    diff_thresh_entry.grid(row=9, column=2, padx=5, sticky = "n")
+
+    perfusion_value.grid(row=11, column=2, padx=5, sticky = "s")
+    per_val_entry.grid(row=12, column=2, padx=5, sticky = "n")
+
+    save.grid(row = 15, column = 2, padx=5)
+
 #finding the filename
 def select_file():
 
@@ -23,28 +49,16 @@ def open_image():
     # Use this code if no image resizing is wanted/needed: img = ImageTk.PhotoImage(file = filename)
     img = ImageTk.PhotoImage(img)
     size = tk.Label(root)
-    size.grid(row = 0, column = 0, columnspan=2, rowspan=35,padx=10,pady=10)
+    size.grid(row = 0, column = 0, columnspan=2, rowspan=40,padx=10,pady=10)
     size.image = img
     size['image'] = img
 
-    threshold_intensity = Label(root, text="Intensity Threshold", bg ='#3A3B3C', fg = 'white')
-    threshold_intensity.grid(row=3, column=2, padx=5)
-    intensity_thresh_entry = Entry(root)
     intensity_thresh_entry.insert(0,'Intensity Threshold') #need to bring over intensity threshold from perfusion.py
-    intensity_thresh_entry.grid(row=4, column=2, padx=5)
 
-    diff_threshold = Label(root, text="Difference Threshold",bg ='#3A3B3C', fg = 'white')
-    diff_threshold.grid(row=6, column=2, padx=5)
-    diff_thresh_entry = Entry(root)
     diff_thresh_entry.insert(0,'Difference Threshold') #need to bring over difference threshold from perfusion.py
-    diff_thresh_entry.grid(row=7, column=2, padx=5)
 
-    perfusion_value = Label(root, text="Perfusion value",bg ='#3A3B3C', fg = 'white')
-    perfusion_value.grid(row=9, column=2, padx=5)
-    per_val_entry = Entry(root)
     per_val_entry.insert(0,'Perfusion value') #need to bring over perfusion value from perfusion.py
-    per_val_entry.grid(row=10, column=2, padx=5)
-    
+
 if __name__ == '__main__':
     perIndex = 55.5
 
@@ -52,19 +66,26 @@ if __name__ == '__main__':
     root.geometry("706x534")
     root.title('Testing GUI')
     root.configure(bg='#3A3B3C')
-
-    intro = tk.Label(root, text = 'Upload Files and Display', bg ='#3A3B3C', fg = 'white')
-    intro.grid(row = 0, column = 2, padx=5)
-
-    upload = tk.Button(root, text = 'Upload Files', width = 20, bg ='#3A3B3C', fg = 'white', command =lambda:open_image())
-    upload.grid(row = 1, column = 2, padx=5)
     
+    threshold_intensity = Label(root, text="Intensity Threshold", bg ='#3A3B3C', fg = 'white')
+    intensity_thresh_entry = Entry(root)
+
+    diff_threshold = Label(root, text="Difference Threshold",bg ='#3A3B3C', fg = 'white')
+    diff_thresh_entry = Entry(root)
+
+    perfusion_value = Label(root, text="Perfusion value",bg ='#3A3B3C', fg = 'white')
+    per_val_entry = Entry(root)
+
+    save = tk.Button(root, text = 'Save Files', width = 20, bg ='#3A3B3C', fg = 'white') #doesn't do anything at the moment
+
+    start_window()
+
     root.mainloop() #allows the window to stay open
 
 """ 
 To Do:
     i) Rectangular Image Select - John
-    ii) Grid Layout understanding - Brady
+    ii) Grid Layout understanding - Bradie
     iii) Save the GUI info
     1) Add adjustable threshold values with a default to start (DT & IT)
     2) Add the perfusion value
