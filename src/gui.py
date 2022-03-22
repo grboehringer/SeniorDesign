5b9ba84eeb2d7fa131b450e5397c40b252b1037e
grboehringer <g2boehringer@gmail.com>
2022-03-22 09:14:48 -0500

    rename

--- src/gui.py
+++ src/gui.py
@@ -1,146 +1,220 @@
-from functools import update_wrapper
-import tkinter as tk
-from tkinter import *
-from tkinter.filedialog import askopenfilename
-from turtle import left, right
-from PIL import ImageTk
-import PIL.Image
+from perfusion import *     # Connects Perfusion.py file
+import tkinter as tk        # Imports tkinter library
+from tkinter import *       # Imports tkinter functions
+from PIL import Image, ImageTk  # Image Import
+from tkinter.filedialog import askopenfilename  # Get filename
 
-def start_window():
-    intial_img = PIL.Image.open("images/Instructions.jpg")
-    intial_img = intial_img.resize((512,512))
-    intial_img = ImageTk.PhotoImage(intial_img)
-    size = tk.Label(root)
-    size.grid(row = 0, column = 0, columnspan=2, rowspan=40,padx=10,pady=10)
-    size.image = intial_img
-    size['image'] = intial_img
+class Window(Frame):
 
-    intro = tk.Label(root, text = ' ', bg ='#3A3B3C', fg = 'white')
-    intro.grid(row = 0, column = 2, padx=5, sticky = "s")
+    def __init__(self, master=None):
+        """Set up class and top menu."""
+        Frame.__init__(self, master)
 
-    upload = tk.Button(root, text = 'Upload Files', width = 20, bg ='#3A3B3C', fg = 'white', command =lambda:open_image())
-    upload.grid(row = 1, column = 2, padx=5, sticky = "n")
+        self.master = master
+        self.pos = []
+        self.master.title("Perfusion Index")
+        self.pack(fill=BOTH, expand=True)
 
-    id.grid(row = 5, column = 2, padx=5, sticky = "s")
-    patient_ID.grid(row = 6, column = 2, padx=5,sticky = "n")
-    patient_ID.insert(0,"Enter Patient ID")
+        self.counter = 0
 
-    gain_val.grid(row=7, column=2, padx=5, sticky = "s")
-    gain_val_entry.grid(row=8, column=2, padx=5, sticky = "n")
-    gain_val_entry.insert(0,"Enter Gain Value")
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
 
-    threshold_intensity.grid(row=12, column=2, padx=5, sticky = "s")
-    intensity_thresh_entry.grid(row=13, column=2, padx=5, sticky = "n")
+        """File drop down."""
+        file = Menu(menu)
+        menu.add_cascade(label="File", menu=file)
+        file.add_command(label="Upload Image", command=self.upload_image)
+        file.add_command(label="Save Image and Data", command=self.save_all)
+        file.add_command(label="Settings", command=self.settings)
+        file.add_command(label="Exit", command=self.client_exit)
+        
+        """Analyze drop down."""
+        analyze = Menu(menu)
+        menu.add_cascade(label="Analyze", menu=analyze)
+        analyze.add_command(label="Region of Interest", command=self.regionOfInterest)
+        analyze.add_command(label="Compare Images", command=self.compare_images)
 
-    diff_threshold.grid(row=14, column=2, padx=5, sticky = "s")
-    diff_thresh_entry.grid(row=15, column=2, padx=5, sticky = "n")
+        id = tk.Label(self, text = 'Patient ID:')
+        id.pack(side = TOP, expand = True, fill = BOTH)
 
-    perfusion_value.grid(row=16, column=2, padx=5, sticky = "s")
-    per_val_entry.grid(row=17, column=2, padx=5, sticky = "n")
+        """Instructions image upload."""
 
-    change_Th.grid(row = 21, column = 2, padx=5)
-    crop.grid(row = 23, column = 2, padx=5)
-    compare.grid(row = 25, column = 2, padx=5)
-    save.grid(row = 27, column = 2, padx=5)
+        load = Image.open("images/Instructions.jpg")
+        render = ImageTk.PhotoImage(load)
+        self.img = Label(self, image=render)
+        self.img.image = render
+        self.img.config(image = render)
+        self.img.pack(side = TOP, expand = True, fill = BOTH)
+        w, h = load.size
 
-#finding the filename
-def select_file():
+        """Figure out what this does!!!"""
+        #self.canvas = Canvas(master, width=w, height=h)
+        #self.canvas.create_image((w/2,h/2),image=render)
+        #self.canvas.pack()
 
-    # define allowed file types
-    f_types = [('Jpg files', '*.jpg'), ('jpeg files', '*.jpeg'), ('PNG files','*.png')]
+        # Try these for image thing
+        # img = PhotoImage(file="python-tkinter-canvas-image-for-use.png")
+        # canvas.create_image(370, 200, image=img)
 
-    # prompt dialog box
-    filename = askopenfilename(filetypes = f_types)
-    #dynamic column variable
-    #image resizing before displaying
-    return filename
+        """Sets window???"""
+        root.geometry("600x600")        # Why does the prefix "%dx%d"% need to be there?
 
-def open_image():
-    global filename
-    filename = select_file()
-    img = PIL.Image.open(filename)
-    img = img.resize((512,512))
-    #image display code
-    # Use this code if no image resizing is wanted/needed: img = ImageTk.PhotoImage(file = filename)
-    img = ImageTk.PhotoImage(img)
-    size = tk.Label(root)
-    size.grid(row = 0, column = 0, columnspan=2, rowspan=40,padx=10,pady=10)
-    size.image = img
-    size['image'] = img
+    """ MENU FUNCTIONS """
+    
+    def upload_image(self):
+        """Open the selected image and resize."""
+        global filename
+        w = 0
+        h = 0
+        self.img.config(image='')
+        filename = self.select_file()
+        load = Image.open(filename)
+        render = ImageTk.PhotoImage(load)
+        self.img.config(image = render)
+        #self.img = Label(self, image=render)
+        self.img.image = render
+        self.img.pack(side = TOP, expand = True, fill = BOTH)
+        w, h = load.size
 
-    threshold_display(filename,intensityThreshold,differenceThreshold)
+        self.canvas = Canvas(root, width=w, height=h)
+        self.canvas.create_image((w/2,h/2),image=render)
+        self.canvas.pack()
 
-def threshold_display(filename,intensityThreshold,differenceThreshold):
-    perfusion = algorithm(filename,intensityThreshold,differenceThreshold)
-    perfusionVal = finalVal(perfusion)
+        #self.threshold_display(filename,intensityThreshold,differenceThreshold)
 
-    intensity_thresh_entry.delete(0, tk.END)
-    diff_thresh_entry.delete(0, tk.END)
-    per_val_entry.delete(0, tk.END)
+    def save_all(self):
+        """Save image and associated data to file"""
+        print('Image and Data Function')
+    
+    def settings(self):
+        """Allow thresholds and machine constants to be entered manually"""
+        # Setup New Window
+        root2 = tk.Tk()
+        root2.title('Settings')
+        root2.configure(bg='#3A3B3C')
 
-    intensity_thresh_entry.insert(0,intensityThreshold)
-    diff_thresh_entry.insert(0,differenceThreshold)
-    per_val_entry.insert(0,perfusionVal)
+        # Initialize User Entries
+        id = tk.Label(root2, text = 'Patient ID:', bg ='#3A3B3C', fg = 'white')
+        patient_ID = Entry(root2)
+        
+        threshold_intensity = Label(root2, text="Intensity Threshold:", bg ='#3A3B3C', fg = 'white')
+        intensity_thresh_entry = Entry(root2)
+        
+        diff_threshold = Label(root2, text="Difference Threshold:",bg ='#3A3B3C', fg = 'white')
+        diff_thresh_entry = Entry(root2)
+        
+        gain_val = Label(root2, text="Gain Value:", bg ='#3A3B3C', fg = 'white')
+        gain_val_entry = Entry(root2)
 
-def change_Thresholds():
-    new_int_thresh = intensity_thresh_entry.get()
-    new_diff_thresh = diff_thresh_entry.get()
-    threshold_display(filename,int(new_int_thresh),int(new_diff_thresh))
+        # Display and Organize User Entries (Label & Box)
+        id.grid(row = 1, column = 1, padx=10, pady=5, sticky='e')
+        patient_ID.grid(row = 1, column = 2, padx=5, pady=5)
+        patient_ID.insert(0,"[Enter Patient ID]")
 
-def save_file():
-    #to do: create file name as patient ID
-    # one file = one patient
-    # add header to file
-    # user can choose which file to open
-    # make it so user can choose where to save it
-    # first save image as sepaerate file, then save them in same file
-    pID = patient_ID.get()
-    diff_thresh = diff_thresh_entry.get()
-    int_thresh = intensity_thresh_entry.get()
-    gain = gain_val_entry.get()
-    per_index = per_val_entry.get()
-    f = open('Ultrasound data', 'a+')
-    f.write('%s\t%s\t%s\t%s\t%s\n'%(pID,diff_thresh,int_thresh,gain,per_index))
-    f.close()
+        gain_val.grid(row=2, column=1, padx=10, pady=5, sticky='e')
+        gain_val_entry.grid(row=2, column=2, padx=5, pady=5)
+        gain_val_entry.insert(0,"[Enter Gain Value]")
+
+        threshold_intensity.grid(row=3, column=1, padx=10, pady=5, sticky='e')
+        intensity_thresh_entry.grid(row=3, column=2, padx=5, pady=5)
+
+        diff_threshold.grid(row=4, column=1, padx=10, pady=5, sticky='e')
+        diff_thresh_entry.grid(row=4, column=2, padx=5, pady=5)
+
+        # x = variable.get() can store entry
+
+    def client_exit(self):
+        """Exit program."""
+        exit()
+    
+    def regionOfInterest(self):
+        """Select ROI."""
+        root.config(cursor="plus")
+        self.canvas.bind("<Button-1>", self.imgClick)
+
+    def compare_images(self):
+        """Compare Images in new window"""
+        print('Compare Images Function')
+
+    """ SUBFUNCTIONS """
+
+    def threshold_display(filename,intensityThreshold,differenceThreshold):
+        """Update threshold values, perfusion value, and gain when changed by user."""
+        perfusion = algorithm(filename,intensityThreshold,differenceThreshold)
+        perfusionVal = finalVal(perfusion)
+
+
+        #intensity_thresh_entry.insert(0,intensityThreshold)
+
+        #diff_thresh_entry.insert(0,differenceThreshold)
+
+        #gain_val_entry.insert(0,"Enter Gain Value")
+
+        #per_val_entry.insert(0,perfusionVal)
+
+    def select_file(self):
+        """Finds the filename."""
+        # define allowed file types
+        f_types = [('Jpg files', '*.jpg'), ('jpeg files', '*.jpeg'), ('PNG files','*.png')]
+
+        # prompt dialog box
+        filename = askopenfilename(filetypes = f_types)
+
+        return filename
+
+    def imgClick(self, event):
+        """Selection counter, displays rectangle, and stores selection values"""
+        if self.counter < 2:
+            x = self.canvas.canvasx(event.x)
+            y = self.canvas.canvasy(event.y)
+            self.pos.append((x, y))
+            print(self.pos)
+            self.canvas.create_line(x - 5, y, x + 5, y, fill="red", tags="crosshair")
+            self.canvas.create_line(x, y - 5, x, y + 5, fill="red", tags="crosshair")
+            self.counter += 1
+
+        else:
+            self.canvas.create_rectangle(self.pos[0][0], self.pos[0][1], self.pos[1][0], self.pos[1][1], outline="red", tags="crosshair")
+            
+            # Save Coordinates Window Setup
+            self.save_coordinates = Tk()
+            self.save_coordinates.title('Save Selection')
+            self.save_coordinates.configure(bg='#3A3B3C')
+            
+            # Disable closing the window
+            self.save_coordinates.protocol("WM_DELETE_WINDOW", self.disable_event)
+            # ADD RESIZING LOCK
+            
+            coord_msg = tk.Label(self.save_coordinates, text = 'Would you like to save this selection?', bg ='#3A3B3C', fg = 'white')
+            coord_msg.grid(row = 1, column = 1, columnspan = 2, padx=10, pady=5, ipady=5)
+            
+            save_sel = tk.Button(self.save_coordinates, text = "Save Selection", width = 15, bg ='#3A3B3C', fg = 'white',command =self.save_coord)
+            save_sel.grid(row = 2, column = 1, padx=5, pady=5)
+            
+            del_sel = tk.Button(self.save_coordinates, text = "Delete Selection", width = 15, bg ='#3A3B3C', fg = 'white',command =self.delete_coord)
+            del_sel.grid(row = 2, column = 2, padx=5, pady=5)
+
+    def save_coord(self):
+        """Save selection coordinates and crop image for perfusion"""
+        print("save coord and crop")
+        self.save_coordinates.destroy()
+        # RERUN ALGORITHM HERE TO GET NEW PV WITH CROPPED IMAGE
+
+    def delete_coord(self):
+        """Delete selection coordinates"""
+        self.canvas.delete("crosshair")
+        self.pos = []
+        self.counter = 0
+        self.canvas.unbind("<Button 1>")
+        root.config(cursor="arrow")
+        self.save_coordinates.destroy()
+
+    def disable_event(self):
+        pass
+
 
 if __name__ == '__main__':
-    from perfusion import *
-    from crop import *
-    perIndex = 55.5
 
-    root = tk.Tk()
-    root.geometry("706x534")
-    root.title('Perfusion Index')
-    root.configure(bg='#3A3B3C')
-
-    id = tk.Label(root, text = 'Patient ID', bg ='#3A3B3C', fg = 'white')
-    patient_ID = Entry(root)
-    threshold_intensity = Label(root, text="Intensity Threshold", bg ='#3A3B3C', fg = 'white')
-    intensity_thresh_entry = Entry(root)
-    diff_threshold = Label(root, text="Difference Threshold",bg ='#3A3B3C', fg = 'white')
-    diff_thresh_entry = Entry(root)
-    gain_val = Label(root, text="Gain Value", bg ='#3A3B3C', fg = 'white')
-    gain_val_entry = Entry(root)
-    perfusion_value = Label(root, text="Perfusion Index Value",bg ='#3A3B3C', fg = 'white')
-    per_val_entry = Entry(root)
-
-    
-    change_Th = tk.Button(root, text = "Change Threshold Values", width = 20, bg ='#3A3B3C', fg = 'white',command =lambda:change_Thresholds())
-    crop = tk.Button(root, text = 'Crop Image', width = 20, bg ='#3A3B3C', fg = 'white') #doesn't do anything at the moment
-    compare = tk.Button(root, text = 'Compare Images', width = 20, bg ='#3A3B3C', fg = 'white') #doesn't do anything at the moment
-    save = tk.Button(root, text = 'Save Files', width = 20, bg ='#3A3B3C', fg = 'white',command =lambda:save_file()) #doesn't do anything at the moment
-
-    start_window()
-
-    root.mainloop() #allows the window to stay open
-
-""" 
-To Do:
-
-    1) Add adjustable threshold values with a default to start (DT & IT)
-    2) Save GUI info and Image/Video
-    3) Cropping Image and saving new image with GUI info
-    4) Allow for PI comparison
-    5) Import video and plot PIs
-    6) Make GUI look appealing (buttons all on one side, items properly spaced, etc.)
-    """
+    root = Tk()
+    app = Window(root)
+    root.mainloop() 
